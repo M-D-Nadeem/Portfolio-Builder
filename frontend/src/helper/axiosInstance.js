91aec0c320b1920e8c569527d18f3cb5a7b2fb04
M-D-Nadeem <149703432+M-D-Nadeem@users.noreply.github.com>
2025-02-27 00:39:41 +0530

    Commiting 1st version

--- frontend/src/helper/axiosInstance.js
+++ frontend/src/helper/axiosInstance.js
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const axiosInstance=axios.create()
 
-axiosInstance.defaults.baseURL="http://localhost:8001"
+// axiosInstance.defaults.baseURL="http://localhost:8001"
+axiosInstance.defaults.baseURL="https://portfolio-builder-backend-ochre.vercel.app"
 axiosInstance.defaults.withCredentials=true
 
 export default axiosInstance
